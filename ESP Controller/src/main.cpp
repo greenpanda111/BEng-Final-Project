f1d64c0e7bec17fe8be790445b01665539795231
Ben Wiltshire <benjamin.wiltshire@outlook.com>
2025-04-28 17:48:23 +0100

    Mode Switching

--- ESP Controller/src/main.cpp
+++ ESP Controller/src/main.cpp
@@ -1,14 +1,20 @@
 #include <WiFi.h>
 
 // WiFi credentials
-const char* ssid = "wheelchair";
-const char* password = "wheelchair123";
+const char *ssid = "wheelchair";
+const char *password = "wheelchair123";
 
 // Button pin macros
-#define BUTTON_FORWARD_PIN   13
-#define BUTTON_BACKWARD_PIN  12
-#define BUTTON_LEFT_PIN      14
-#define BUTTON_RIGHT_PIN     27
+#define BUTTON_ONE 13
+#define BUTTON_TWO 12
+#define BUTTON_THREE 14
+#define BUTTON_FOUR 27
+#define FOUR_SWITCH_TOGGLE 26
+#define TWO_SWITCH_TOGGLE 25
+#define SINGLE_SWITCH_TOGGLE 33
+#define JOYSTICK_TOGGLE 32
+#define JOYSTICK_X 35
+#define JOYSTICK_Y 34
 
 // Server IP and port
 IPAddress serverIP(10, 42, 0, 1);
@@ -16,17 +22,26 @@ const uint16_t serverPort = 5001;
 
 WiFiClient client;
 
-void setup() {
+int mode = 0;
+
+void setup()
+{
   Serial.begin(115200);
 
-  pinMode(BUTTON_FORWARD_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_BACKWARD_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_LEFT_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_RIGHT_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_ONE, INPUT_PULLUP);
+  pinMode(BUTTON_TWO, INPUT_PULLUP);
+  pinMode(BUTTON_THREE, INPUT_PULLUP);
+  pinMode(BUTTON_FOUR, INPUT_PULLUP);
+  pinMode(FOUR_SWITCH_TOGGLE, INPUT_PULLDOWN);
+  pinMode(TWO_SWITCH_TOGGLE, INPUT_PULLDOWN);
+  pinMode(SINGLE_SWITCH_TOGGLE, INPUT_PULLDOWN);
+  pinMode(JOYSTICK_TOGGLE, INPUT_PULLDOWN);
+  pinMode(JOYSTICK_X, INPUT);
+  pinMode(JOYSTICK_Y, INPUT);
 
-  //WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.println("Connecting to WiFi...");
   }
@@ -35,9 +50,12 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  if (!client.connected()) {
-    if (!client.connect(serverIP, serverPort)) {
+void loop()
+{
+  if (!client.connected())
+  {
+    if (!client.connect(serverIP, serverPort))
+    {
       Serial.println("Connection to host failed");
       delay(1000);
       return;
@@ -45,28 +63,89 @@ void loop() {
     Serial.println("Connected to server");
   }
 
-  if (digitalRead(BUTTON_FORWARD_PIN) == LOW) {
-    client.write('F');
-    Serial.println("Sent: F");
-    delay(200);
+  if (digitalRead(FOUR_SWITCH_TOGGLE) == HIGH)
+  {
+    mode = 0;
+  }
+  else if (digitalRead(TWO_SWITCH_TOGGLE == HIGH))
+  {
+    mode = 1;
+  }
+  else if (digitalRead(SINGLE_SWITCH_TOGGLE == HIGH))
+  {
+    mode = 2;
+  }
+  else if (digitalRead(JOYSTICK_TOGGLE == HIGH))
+  {
+    mode = 3;
   }
 
-  if (digitalRead(BUTTON_BACKWARD_PIN) == LOW) {
-    client.write('B');
-    Serial.println("Sent: B");
-    delay(200);
-  }
+  switch (mode)
+  {
+  case 0:
+    client.write("0");
+    if (digitalRead(BUTTON_ONE) == LOW)
+    {
+      client.write('F');
+      Serial.println("Sent: F");
+    }
 
-  if (digitalRead(BUTTON_LEFT_PIN) == LOW) {
-    client.write('L');
-    Serial.println("Sent: L");
-    delay(200);
-  }
+    if (digitalRead(BUTTON_TWO) == LOW)
+    {
+      client.write('B');
+      Serial.println("Sent: B");
+    }
 
-  if (digitalRead(BUTTON_RIGHT_PIN) == LOW) {
-    client.write('R');
-    Serial.println("Sent: R");
-    delay(200);
+    if (digitalRead(BUTTON_THREE) == LOW)
+    {
+      client.write('L');
+      Serial.println("Sent: L");
+    }
+
+    if (digitalRead(BUTTON_FOUR) == LOW)
+    {
+      client.write('R');
+      Serial.println("Sent: R");
+    }
+
+    else
+    {
+      client.write('s');
+    }
+    break;
+
+  case 1:
+    client.write("1");
+    if (digitalRead(BUTTON_ONE) == LOW & digitalRead(BUTTON_TWO) == LOW)
+    {
+      client.write('F');
+      Serial.println("Sent: F");
+    }
+    else if (digitalRead(BUTTON_ONE) == LOW)
+    {
+      client.write("LF");
+      Serial.println("Sent: LF");
+    }
+    else if (digitalRead(BUTTON_TWO) == LOW)
+    {
+      client.write("RF");
+      Serial.println("Sent: RF");
+    }
+    break;
+
+  case 2:
+    client.write("2");
+    if (digitalRead(BUTTON_ONE) == LOW)
+    {
+      client.write('F');
+    }
+
+  case 3:
+    client.write("3");
+    client.write(analogRead(JOYSTICK_X));
+    client.write(analogRead(JOYSTICK_Y));
+
+    break;
   }
 
   delay(10); // debounce and CPU relaxation
